a87733984857ee4d8da3b25de6f683f6c6e7b60f
DPr1son <77262664+DPr1son@users.noreply.github.com>
2021-01-11 13:42:42 +0300

    Update yandex_drive_searcher_windows.py

--- yandex_drive_searcher_windows.py
+++ yandex_drive_searcher_windows.py
@@ -6,7 +6,7 @@ g = str("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890-_")
 
 def gen():
 	global g
-	i = str("http://yadi.sk/" + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g))
+	i = str("http://yadi.sk/d/" + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g))
 	check(i)
 def check(url):
 	r = requests.get(url)
